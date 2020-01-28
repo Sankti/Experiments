33786a06fb2ccc7d260d75b38336987f9217985a
Adam <57396704+Sankti@users.noreply.github.com>
2020-01-28 12:22:16 +0100

    Add files via upload
    Squaring by repetitive action

---
+++ squaring.py
@@ -0,0 +1,10 @@
+# Squaring by repetitive action
+
+x = 9
+ans = 0
+
+itersLeft = x
+while (itersLeft != 0):
+    ans = ans + x
+    itersLeft = itersLeft - 1
+print(str(x) + '*' + str(x) + ' = ' + str(ans))
