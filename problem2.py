6cb09fda70b01040ef08af8f3de1119016494724
Adam <57396704+Sankti@users.noreply.github.com>
2020-01-30 11:25:11 +0100

    Update problem2.py

--- problem2.py
+++ problem2.py
@@ -5,10 +5,10 @@ range_letters = 0
 count = 0
 
 for letter in s:
+    range_letters = current_letter + 3
+    print(s[current_letter:range_letters])
+    if s[current_letter:range_letters] == "bob":
+        count += 1
     current_letter += 1
-    range_letters = current_letter + 2
-    while letter == "b":
-        if range(current_letter, range_letters) == "bob":
-            count += 1
 
 print("Number of times bob occurs is: " + str(count))
